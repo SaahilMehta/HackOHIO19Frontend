280dc0db15f67a9c404ababdaa2890363b9e3bb9
Connor Finneran <finneran.61@osu.edu>
2019-11-02 14:14:12 -0400

    add marker to map functionality, not tested at all, no icons involved

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,13 @@
  * limitations under the License.
  */
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'map_markers.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -26,29 +31,83 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   GoogleMapController mapController;
-
   final LatLng _center = const LatLng(45.521563, -122.677433);
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  MarkerId currentMarker;
+  int idCounter = 1;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
+  void _addMarker() {
+    final int markerCount = markers.length;
+    final String idValue = 'marker_id_$idCounter';
+    idCounter++;
+    final MarkerId markerId = MarkerId(idValue);
+
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: _center, //fix me
+      infoWindow: InfoWindow(title: "placeholder"),
+      //add functions to drag/tap/whatever here
+    );
+
+    setState(() {
+      markers[markerId] = marker;
+    });
+  }
+  
+  void _deleteMarker() {
+    setState(() {
+      if (markers.containsKey(currentMarker)) {
+        markers.remove(currentMarker);
+      }
+    });
+  }
+
+//    mapController.addMarker(
+//      MarkerOptions(
+//        position: center, //fix me
+//        infoWindowText: InfoWindow("title", "content"),
+//        icon: FontAwesomeIcons.mapPin,
+//      ),
+//    );
+
   @override
   Widget build(BuildContext context) {
+    Widget thumbTack = IconButton(
+      // Use the FontAwesomeIcons class for the IconData
+        icon: Icon(FontAwesomeIcons.mapPin),
+        onPressed: () { print("Pressed"); }
+    );
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
+        body: Stack(
+          children: [
+          GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 11.0,
           ),
+          markers: Set<Marker>.of(markers.values),
         ),
+          Positioned(
+            top: 40,
+            right: 20,
+            child: FlatButton(
+              child: Text('add'),
+              onPressed: _addMarker,
+            ),
+          )
+        ]
       ),
+      )
     );
   }
 }
