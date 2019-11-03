e9e79ff364149490be1f218d1cb3762b8ebd2d4a
Connor Finneran <finneran.61@osu.edu>
2019-11-03 11:59:08 -0500

    added lots of features, not working

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'dart:core';
 import 'dart:async';
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart' as prefix0;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
@@ -20,23 +22,29 @@ class MyHomePage extends State<MyHome> {
   bool userSelectingPosition = false;
   // AppState appState = AppState();
   GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(40, -83);
   Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
   MarkerId currentMarker;
   int idCounter = 1;
-  String url = "http://8fd924eb.ngrok.io";
+  String url = "http://57c2619e.ngrok.io";
   Future<Pins> pins;
+  bool showVoting = false;
+  int currentTotal = 0;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
-
+    if (markers.containsKey(currentMarker)) {
+        final Marker resetMarker = markers[currentMarker].copyWith(iconParam:
+        BitmapDescriptor.defaultMarker);
+        markers[currentMarker] = resetMarker;
+    }
     // Fill in points from API response
     String uri = url + "/pins";
 
     var res = http
         .get(uri)
         .then((res) => Pins.fromJson(json.decode(res.body)).pins.forEach(
-          (pin) => _putMarker(LatLng(pin.latitude, pin.longitude), pin.title)));
+          (pin) => _putMarker(LatLng(pin.latitude, pin.longitude), pin.title, pin.description)));
   }
 
   void _onMarkerTap(MarkerId mid, LatLng location) {
@@ -54,15 +62,17 @@ class MyHomePage extends State<MyHome> {
       ));
       markers[mid] = newMarker;
     });
+    setState(() {
+      showVoting = true;
+    });
     mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
       target: location,
       zoom: 15.0,
     )));
-//    Navigator.push(context, MaterialPageRoute(builder: (context) => PinInfo()));
   }
 
   void _addMarker(LatLng location, List<String> pinData) async {
-    _putMarker(location, pinData[0]);
+    _putMarker(location, pinData[0], pinData[2]);
 
     String latitude = pinData[3]
         .substring(pinData[3].indexOf('(') + 1, pinData[3].indexOf(','));
@@ -73,7 +83,7 @@ class MyHomePage extends State<MyHome> {
     // print("$latitude, $longitude");
 
     // HTTP POST to backend
-    var url = "http://8fd924eb.ngrok.io/pins";
+    var url = "http://57c2619e.ngrok.io/pins";
     var response = await http.post(url, headers:{"Content-Type":"application/json"} ,body: utf8.encode(json.encode({
       'title': pinData[0],
 //      'tags': pinData[1],
@@ -89,20 +99,17 @@ class MyHomePage extends State<MyHome> {
     print("$pinData");
   }
 
-  void _putMarker(LatLng location, String title) async {
-    final int markerCount = markers.length;
+  void _putMarker(LatLng location, String title, String description) async {
     idCounter++;
     final MarkerId markerId = MarkerId(title);
-    var description = "test description here";
 
     final Marker marker = Marker(
       markerId: markerId,
       position: location,
-      infoWindow: InfoWindow(title: "marker $markerCount", snippet: description),
+      infoWindow: InfoWindow(title: title, snippet:  description),
       onTap: () {
         _onMarkerTap(markerId, location);
       },
-      //add functions to drag/tap/whatever here
     );
 
     setState(() {
@@ -121,36 +128,55 @@ class MyHomePage extends State<MyHome> {
     });
   }
 
-  _determineTapPosition(LatLng argument) async {
+  void _resetGlobals() {
+    setState(() {
+      showVoting = false;
+      currentTotal = 0;
+      if (markers.containsKey(currentMarker)) {
+        final Marker resetMarker = markers[currentMarker].copyWith(iconParam:
+        BitmapDescriptor.defaultMarker);
+        markers[currentMarker] = resetMarker;
+      }
+    });
+  }
+
+  void _determineTapPosition(LatLng argument) async {
     if (userSelectingPosition) {
       userSelectingPosition = false;
       final result = await Navigator.push(context,
           MaterialPageRoute(builder: (context) => PinTextInput(argument)));
       _addMarker(argument, result);
 
-      // print('DEBUG------');
-      // for (var i = 0; i < result.length; i++) {
-      //   print("$result[i]\n");
-      // }
-
+    } else {
+      _resetGlobals();
     }
   }
 
-//    mapController.addMarker(
-//      MarkerOptions(
-//        position: center, //fix me
-//        infoWindowText: InfoWindow("title", "content"),
-//        icon: FontAwesomeIcons.mapPin,
-//      ),
-//    );
+  void _updateVoting(int upvotes, int downvotes, MarkerId mid) async {
+    // get pin from API response
+    var res, i;
+    var markerList = markers.values.toList();
+    for (i = 0; i < markerList.length; i++) {
+      if (markerList[i] == markers[mid]) break;
+    }
+    if (upvotes > 0) {
+      String uri = url + "/pins/$i/upvote";
+      res = await http.get(uri);
+    }
+    if (downvotes > 0) {
+      String uri = url + "/pins/$i/downvote";
+      res = await http.get(uri);
+    }
+    String uri = url + "/pins/$i";
+    res = await http.get(uri);
+    var pin = Pin.fromJson(json.decode(res.body));
+    setState(() {
+     currentTotal = pin.totalScore;
+    }); 
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Widget thumbTack = IconButton(
-    //   // Use the FontAwesomeIcons class for the IconData
-    //     icon: Icon(FontAwesomeIcons.mapPin),
-    //     onPressed: () { print("Pressed"); }
-    // );
     return Scaffold(
         appBar: AppBar(
           title: Text('Pin App'),
@@ -166,6 +192,7 @@ class MyHomePage extends State<MyHome> {
             markers: Set<Marker>.of(markers.values),
             onTap: _determineTapPosition,
           ),
+          !showVoting ? Row() : _buildVotingContainer(Colors.black, FontAwesomeIcons.thumbsDown, FontAwesomeIcons.thumbsUp, currentTotal, currentMarker)
         ]),
         drawer: Drawer(
             child: ListView(padding: EdgeInsets.zero, children: <Widget>[
@@ -204,4 +231,33 @@ class MyHomePage extends State<MyHome> {
           child: const Icon(Icons.add),
         ));
   }
+
+Row _buildVotingContainer(Color color, IconData downIcon, IconData upIcon, int total_votes, MarkerId mid) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      IconButton(
+        icon: Icon(downIcon), 
+        color: color,
+        onPressed: () {
+          _updateVoting(0, 1, mid);
+        }
+      ),
+      Container(
+        margin: const EdgeInsets.all(10),
+        child: Text(
+          "Total Votes: $currentTotal",
+        )
+      ),
+      IconButton(
+        icon: Icon(upIcon), 
+        color: color,
+        onPressed: () {
+          _updateVoting(1, 0, mid);
+        }
+      ),
+    ],
+  );
+}
 }
